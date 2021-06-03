9e392e464d699e4763023b2074b9b72d13ea120d
Marc Garcia <marcgarciacampama@gmail.com>
2021-06-03 20:50:12 +0200

    poner nombres y poner imagen en mensaje

--- js/index.js
+++ js/index.js
@@ -18,6 +18,13 @@ for (const personaje of personajes) {
   const morir = molde.querySelector(".accion:nth-child(2)");
   const emojiVivo = molde.querySelector(".estado:nth-child(2)");
   const emojiMuerto = molde.querySelector(".estado:first-child");
+  const nombrePersonaje = molde.querySelector(".nombre")
+
+  const imagenMensaje = document.querySelector(".comunicaciones > img");
+
+  const imagenTarjeta = `img/${nombre.toLowerCase()}.jpg`;
+
+  nombrePersonaje.textContent = `${nombre} ${personaje.familia}`
 
   emojiMuerto.classList.add("d-none");
   morir.addEventListener("click", () => {
@@ -34,11 +41,12 @@ for (const personaje of personajes) {
   hablar.addEventListener("click", () => {
     comunicacion.classList.add("on");
     fraseComunicaion.textContent = personaje.comunicar();
+    imagenMensaje.src = imagenTarjeta;
     setTimeout(() => {
       comunicacion.classList.remove("on");
     }, 2000);
   });
-  imagenPersonaje.src = `img/${nombre.toLowerCase()}.jpg`;
+  imagenPersonaje.src = imagenTarjeta;
   molde.classList.remove("personaje-dummy");
   console.log(molde);
   padreDelMolde.append(molde);
--- js/personajes.js
+++ js/personajes.js
@@ -8,7 +8,7 @@ import Asesor from "../datos/Asesor.js";
 const jaime = new Luchador("Jaime", "Lannister", 40, "espadon");
 const daenerys = new Luchador("Daenerys", "Lannister", 40, "guantazo");
 const joffrey = new Rey("Joffrey", "Baratheon", 17, 3);
-const bronn = new Escudero("Bronn", "NuNAFamily", 78);
+const bronn = new Escudero("Bronn", "", 78);
 const tyrion = new Asesor("Tyrion", "Lannister", 36, "ballesta");
 const tywin = new Asesor("Tywin", "Lannister", 89, "ninguna");
 const personajes = [tyrion, tywin, joffrey, jaime, daenerys, bronn];
