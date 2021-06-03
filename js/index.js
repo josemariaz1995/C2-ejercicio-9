8d3af5901f794a303180206acc139d70e702e0ed
Marc Garcia <marcgarciacampama@gmail.com>
2021-06-03 21:33:07 +0200

    cambio emojis por cada clase y añadir funcion ocultar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable linebreak-style */
 // eslint-disable-next-line import/extensions
+import Asesor from "../datos/Asesor.js";
+import Escudero from "../datos/Ecudero.js";
+import Luchador from "../datos/Luchador.js";
+import Rey from "../datos/Rey.js";
 import personajes from "./personajes.js";
 
 for (const personaje of personajes) {
@@ -24,14 +28,31 @@ for (const personaje of personajes) {
 
   const imagenTarjeta = `img/${nombre.toLowerCase()}.jpg`;
 
+  if (personaje instanceof Luchador){
+   emoji.textContent = String.fromCodePoint(0x2694)
+  }
+  else if(personaje instanceof Rey){
+    emoji.textContent = String.fromCodePoint(0x1f451)
+  }
+   else if(personaje instanceof Asesor){
+    emoji.textContent = String.fromCodePoint(0x1f393)
+  }
+   else {emoji.textContent = String.fromCodePoint(0x0001F6E1)}
+  
+
   nombrePersonaje.textContent = `${nombre} ${personaje.familia}`
 
+  const ocultar = (arrayElemento) => {
+    for (const elemento of arrayElemento) {
+      elemento.classList.add("d-none");
+    }
+  };
+  const elementoParaOcultar = [morir, emojiVivo];
   emojiMuerto.classList.add("d-none");
   morir.addEventListener("click", () => {
     personaje.morir();
     imagenPersonaje.classList.add("muerto");
-    morir.classList.add("d-none");
-    emojiVivo.classList.add("d-none");
+    ocultar(elementoParaOcultar);
     emojiMuerto.classList.remove("d-none");
   });
 
